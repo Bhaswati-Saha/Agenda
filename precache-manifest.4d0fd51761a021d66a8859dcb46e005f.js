8be8c059cd401608588cf28351e721c44684c7e2
Bhaswati-Saha <bhaswati.sh12@gmail.com>
2021-06-25 11:06:16 +0530

    Updates

---
+++ precache-manifest.4d0fd51761a021d66a8859dcb46e005f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6278a386905797536aa47444aa66b38",
+    "url": "/Agenda/index.html"
+  },
+  {
+    "revision": "9bda3f93f0f6c0a83ca2",
+    "url": "/Agenda/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5c5fa7c1832e72934fcd",
+    "url": "/Agenda/static/css/main.18a4ef9f.chunk.css"
+  },
+  {
+    "revision": "9bda3f93f0f6c0a83ca2",
+    "url": "/Agenda/static/js/2.b1e74d00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Agenda/static/js/2.b1e74d00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5fa7c1832e72934fcd",
+    "url": "/Agenda/static/js/main.ef5b85f0.chunk.js"
+  },
+  {
+    "revision": "953f3dced141396d3aa7",
+    "url": "/Agenda/static/js/runtime-main.8e9095fa.js"
+  }
+]);
